4e692d358711ecf179b7f7d22b868687ba88fc67
Brandon F <bforys@gmail.com>
2017-10-14 23:19:26 -0700

    Major update!

---
+++ static/scripts/main.js
@@ -0,0 +1,12 @@
+var options = {
+  strings: ["<h3>Arts Psychology.</h3>", "<h3>Web Developer.</h3>", "<h3>I love machine learning.</h3>", "<h3>Arduino Enthusiast.</h3>", "<h3>Geopolitics Nerd.</h3>"],
+  typeSpeed: 40,
+  backSpeed: 40,
+  loop: true,
+  loopCount: Infinity,
+  showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true
+}
+
+var typed = new Typed(".element", options);
