b3a7d4f823abd64558b51eb1dd7cfd83f308f902
Brandon F <bforys@gmail.com>
2017-10-15 22:28:59 -0700

    a cool thing

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -10,3 +10,22 @@ var options = {
 }
 
 var typed = new Typed(".element", options);
+
+var els, i, len, title;
+  var konamiCode = '38,38,40,40,37,39,37,39,66,65';
+  var keyPresses = [];
+  var checkKonami = function(e) {
+    keyPresses.push(e.keyCode);
+    if (keyPresses.slice(keyPresses.length-10).join() === konamiCode) {
+      runKonami();
+    }
+  };
+  var runKonami = function() {
+    els = document.getElementsByTagName('p');
+    for (i = 0, len = els.length; i < len; i++) {
+      title = els[i].textContent || els[i].innerText;
+      title = title.trim();
+      els[i].innerHTML = title.split('').reverse().join('');
+    }
+  };
+  document.addEventListener('keyup', checkKonami);
